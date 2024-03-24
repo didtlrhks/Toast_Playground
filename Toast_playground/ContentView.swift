8fdc98f88fafe40183e269ae0e942273e54b5e90
sikwan <yysskk99999@naver.com>
2024-03-24 22:13:42 +0900

    toast message

--- Toast_playground/ContentView.swift
+++ Toast_playground/ContentView.swift
@@ -4,19 +4,50 @@
 //
 //  Created by 양시관 on 3/24/24.
 //
-
 import SwiftUI
+import PopupView
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State var isPresentedFloating: Bool = false
+  
+  var body: some View {
+    VStack {
+      Button(
+        action: { isPresentedFloating.toggle() },
+        label: {
+          Text("Show Floating")
         }
-        .padding()
+      )
     }
+    .padding()
+    .popup(isPresented: $isPresentedFloating) {
+      FloatingView()
+    } customize: {
+      $0
+        .type(.floater())
+        .position(.bottom)
+        .autohideIn(1.0)
+        .animation(.spring())
+        .closeOnTapOutside(true)
+        .backgroundColor(.clear)
+    }
+  }
+}
+
+
+private struct FloatingView: View {
+  var body: some View {
+    HStack {
+      Spacer()
+      Text("This is Floating")
+        .font(.headline)
+        .padding(.vertical, 10)
+      Spacer()
+    }
+    .background(Color.white)
+    .cornerRadius(20)
+    .padding(.horizontal, 20)
+  }
 }
 
 #Preview {
