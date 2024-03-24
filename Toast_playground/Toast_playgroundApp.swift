32b01eba441ac9381bc05f7fe2b0658160436b80
sikwan <yysskk99999@naver.com>
2024-03-24 21:54:52 +0900

    init

---
+++ Toast_playground/Toast_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toast_playgroundApp.swift
+//  Toast_playground
+//
+//  Created by 양시관 on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Toast_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
